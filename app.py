314d2d381e7fd1412b65597c678b7ecca9633603
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-03-31 13:56:46 -0500

    fix: gunicorn added

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from flask import Flask
+from flask import Flask,request
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/',methods=["POST"])
 def index():
-    return {"message":"hola"}
+    data = dict(request.get_json())
+    return {"text": data["text"], "user" : "Celina"}
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-#mi api para lo de moviles la hare con esto y con el package de fastAPI => queda lindo
-#miraa
