f8364e28c83092be38b34199d2fee0ca45387d99
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-04-12 15:47:34 -0500

    test: trained pipeline added

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask,request
 from flask_cors import CORS
-from spacy import blank
+import spacy
 app = Flask(__name__)
 CORS(app)
-nlp = blank("es")
+nlp = spacy.load("es_core_news_sm")
 
 @app.route('/',methods=["POST"])
 def index():
     data = dict(request.get_json())
     doc = nlp(data["text"])
     for token in doc:
-        print(token.text)
+        print(token.text,token.pos_)
     return {"text": data["text"], "user" : "Celina"}
 
 if __name__ == "__main__":
