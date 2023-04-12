d4b941e9cb04a12a7fb1056426abb500cfbff1f8
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-04-12 13:54:40 -0500

    feat: spacy added

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask,request
 from flask_cors import CORS
+from spacy import blank
 app = Flask(__name__)
 CORS(app)
+nlp = blank("es")
 
 @app.route('/',methods=["POST"])
 def index():
     data = dict(request.get_json())
+    doc = nlp(data["text"])
+    for token in doc:
+        print(token.text)
     return {"text": data["text"], "user" : "Celina"}
 
 if __name__ == "__main__":
